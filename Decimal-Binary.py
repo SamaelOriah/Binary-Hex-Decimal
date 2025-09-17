bbeeb98132bc3737594e765e572d0c8b4e433846
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-17 12:39:05 +0000

    Added a catcher for any binary number that contains any other number than 1 or 0.

--- Decimal-Binary.py
+++ Decimal-Binary.py
@@ -1,5 +1,8 @@
 print("This is a binary to decimal converter. Enter a binary number and it will convert it to a decimal number.")
-binNum=input("\n Enter your binary number: ")
+binNum=input("\nEnter your binary number: ")
+while "2" in binNum:
+    print("Binary numbers can only contain 1s or 0s. Try again.")
+    binNum=input("\nEnter your binary number: ")
 powerNum=len(binNum)-1
 decNum=0
 for x in range(0,len(binNum)-1):
