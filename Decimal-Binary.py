56a815b2022df5ecec3f46c63c62495e885f1208
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-17 12:41:24 +0000

    Amended catcher to properly catch any other numbers

--- Decimal-Binary.py
+++ Decimal-Binary.py
@@ -1,8 +1,19 @@
 print("This is a binary to decimal converter. Enter a binary number and it will convert it to a decimal number.")
 binNum=input("\nEnter your binary number: ")
-while "2" in binNum:
-    print("Binary numbers can only contain 1s or 0s. Try again.")
-    binNum=input("\nEnter your binary number: ")
+oneCount=0
+zeroCount=0
+synCorrect=False
+while synCorrect==False:
+    for x in binNum:
+        if x=="1":
+            oneCount+=1
+        elif x=="0":
+            zeroCount+=1
+    if oneCount+zeroCount!=len(binNum):
+        print("Binary numbers can only contain 1s or 0s. Try again.")
+        binNum=input("\nEnter your binary number: ")
+    else:
+        synCorrect=True
 powerNum=len(binNum)-1
 decNum=0
 for x in range(0,len(binNum)-1):
