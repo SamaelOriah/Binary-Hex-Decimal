4fd57d78bdcd8b83799259e770d4a575fd3a463d
SamaelOriah <106034689+SamaelOriah@users.noreply.github.com>
2025-09-17 12:17:31 +0000

    Created program and tested

--- Decimal-Binary.py
+++ Decimal-Binary.py
@@ -1,1 +1,9 @@
-
+print("This is a binary to decimal converter. Enter a binary number and it will convert it to a decimal number.")
+binNum=input("\n Enter your binary number: ")
+powerNum=len(binNum)-1
+decNum=0
+for x in range(0,len(binNum)-1):
+    if binNum[x]=="1":
+        decNum+=2**powerNum
+    powerNum-=1
+print(f"That number in Decimal is {decNum}.")
